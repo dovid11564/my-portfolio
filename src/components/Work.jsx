3bb29b36803febbde6f915db60ed60d3d036ecbe
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-08-24 10:39:48 -0400

    removed submodule

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,9 +12,104 @@ const Work = () => {
                 </div>
 
                 {/*  container*/}
-                <div 
+                <div
                     className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
-                        {/* grid item - copy this and change it for each project*/}
+                    {/* grid item - copy this and change it for each project*/}
+                    <div style={{ backgroundImage: `url(${RPG})` }}
+                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* hover effetcs */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    {/* this should be a link to a demo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="/">
+                                    {/* this is a link to the repo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ backgroundImage: `url(${RPG})` }}
+                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* hover effetcs */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    {/* this should be a link to a demo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="/">
+                                    {/* this is a link to the repo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ backgroundImage: `url(${RPG})` }}
+                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* hover effetcs */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    {/* this should be a link to a demo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="/">
+                                    {/* this is a link to the repo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ backgroundImage: `url(${RPG})` }}
+                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* hover effetcs */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    {/* this should be a link to a demo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="/">
+                                    {/* this is a link to the repo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ backgroundImage: `url(${RPG})` }}
+                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        {/* hover effetcs */}
+                        <div className="opacity-0 group-hover:opacity-100">
+                            <span className="text-2xl font-bold text-white tracking-wider">
+                                React JS Application
+                            </span>
+                            <div className="pt-8 text-center">
+                                <a href="/">
+                                    {/* this should be a link to a demo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
+                                </a>
+                                <a href="/">
+                                    {/* this is a link to the repo */}
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                     <div style={{ backgroundImage: `url(${RPG})` }}
                     className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
                         {/* hover effetcs */}
