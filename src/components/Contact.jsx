81fb07df31ecb3d7952ccfeb473b76c8834dbb5f
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-08-27 10:43:18 -0400

    cleaned up some content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
                 <form method="POST" action="https://getform.io/f/d5d28891-9ed4-48f5-ac64-7f4ac21177e5" className="flex flex-col max-w-[600px] w-full">
                     <div className="pb-8">
                         <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
-                        <p className="text-gray-300 py-4">Submit the form below or shoot me an email! - insert my email here. </p>
+                        <p className="text-gray-300 py-4">Submit the form below to shoot me an email!</p>
                     </div>
                     <input className='bg-[#ccd6f6] p-2' type="text" placeholder="Name" name='name'></input>
                     <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder="Email" name='email'></input>
