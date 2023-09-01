f420eea6434f17461e7c4d85caffe409e075dcad
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-09-01 17:24:50 -0400

    resized for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,14 @@ const Home = () => {
 
             {/* container */}
             <div className='w-full grid lg:grid-cols-2 place-items-center py-8'>
-              <img src={Profile} alt='Profile of Dovid Edelkopf' className='rounded-full max-w-lg mt-12'/>  
+              <img src={Profile} alt='Profile of Dovid Edelkopf' className=' rounded-full w-64 lg:w-auto mt-12'/>  
+              {/* rounded-full mt-12 w-64 md:max-w-lg */}
+              {/* rounded-full max-w-lg mt-12 */}
             <div className="m-w-[1000px]">
                 <p className="text-yellow">Hi, my name is</p>
                 <h1 className='text-4xl sm:7xl font-bold text-[#ccd6f6]'>Dovid Edelkopf</h1>
-                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack developer.</h2>
-                <p className="text-[#8892b0] py-4 max-w-[700px]">This is where I give a brief summary about what I do and offer</p>
+                <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Full Stack Developer.</h2>
+                <p className="text-[#8892b0] py-4 max-w-[700px]">Please check out my work!</p>
                 <div>
                     <button className="text-yellow group border-2 px-6 py-3 my-2 flex items-center hover:text-green hover:bg-yellow hover:border-yellow">
                         View Work
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import RPG from '../assets/projects/VGImage.png';
 const Work = () => {
     return (
         // might be a good idea to change the text colors not from yellow if I keep this gradient in 
-        <div name='work' className="w:full md:h-screen text-yellow bg-green">
+        <div name='work' className="w:full md:h-screen text-yellow bg-green px-44">
             <div className="m-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 text-yellow border-yellow">
@@ -14,7 +14,7 @@ const Work = () => {
 
                 {/*  container*/}
                 <div
-                    className="grid sm:grid-cols-2 md:grid-cols-4 gap-4">
+                    className="grid grid-cols-2 md:grid-cols-4 gap-4">
                     {/* grid item - copy this and change it for each project*/}
                     <div style={{ backgroundImage: `url(${RPG})` }}
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
