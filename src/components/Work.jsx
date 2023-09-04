6b898380b4aed1b95c287437bd2eaf1f4a5069bc
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2023-09-04 11:34:02 -0400

    redid project cards

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,8 +3,8 @@ import RPG from '../assets/projects/VGImage.png';
 const Work = () => {
     return (
         // might be a good idea to change the text colors not from yellow if I keep this gradient in 
-        <div name='work' className="w:full md:h-screen text-yellow bg-green px-44   ">
-            <div className="m-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div name='work' className="w-full md:h-screen text-yellow bg-green  lg:px-44   ">
+            <div className="p-4 flex flex-col justify-center w-full h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 text-yellow border-yellow">
                         Work
@@ -14,67 +14,72 @@ const Work = () => {
 
                 {/*  container*/}
                 <div
-                    className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                    className="grid grid-cols-1 md:grid-cols-3 gap-16 ">
                     {/* grid item - copy this and change it for each project*/}
                     <div style={{ backgroundImage: `url(${RPG})` }}
-                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:border-2 hover:border-yellow">
                         {/* hover effetcs */}
                         <div className="opacity-0 group-hover:opacity-100">
-                            <span className="text-2xl font-bold text-white tracking-wider">
-                                React JS Application
+                            <span className="text-2xl font-bold text-yellow tracking-wider border-b-2 border-yellow">
+                                Surfwatch
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
+                                <a href="https://exquisite-platypus-07e5f2.netlify.app"
+                                target="_blank">
                                     {/* this should be a link to a demo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-lg">Demo</button>
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Link</button>
                                 </a>
-                                <a href="/">
+                                <a href="https://github.com/dovid11564/surfwatch"
+                                target="_blank">
                                     {/* this is a link to the repo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-lg">Code</button>
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Code</button>
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div style={{ backgroundImage: `url(${RPG})` }}
-                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                        className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:border-2 hover:border-yellow">
                         {/* hover effetcs */}
                         <div className="opacity-0 group-hover:opacity-100">
-                            <span className="text-2xl font-bold text-white tracking-wider">
-                                React JS Application
+                            <span className="text-2xl font-bold text-yellow tracking-wider border-b-2 border-yellow">
+                                MetroLine
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
-                                    {/* this should be a link to a demo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-lg">Demo</button>
-                                </a>
-                                <a href="/">
+                                {/* <a href="/">
+                                    {/* this should be a link to a demo 
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Demo</button>
+                                </a> */}
+                                <a href="https://github.com/dovid11564/train-project-phase-2"
+                                target="_blank">
                                     {/* this is a link to the repo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-lg">Code</button>
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Code</button>
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div style={{ backgroundImage: `url(${RPG})` }}
-                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+                    className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div hover:border-2 hover:border-yellow">
                         {/* hover effetcs */}
                         <div className="opacity-0 group-hover:opacity-100">
-                            <span className="text-2xl font-bold text-white tracking-wider">
-                                React JS Application
+                            <span className="text-2xl font-bold text-yellow tracking-wider border-b-2 border-yellow">
+                                A Chassidishe RPG
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
-                                    {/* this should be a link to a demo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-green font-bold text-lg">Demo</button>
-                                </a>
-                                <a href="/">
+                                {/* <a href="/">
+                                    {/* this should be a link to a demo 
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Demo</button>
+                                </a> */}
+                                <a href="https://github.com/dovid11564/RPG"
+                                target="_blank"
+                                >
                                     {/* this is a link to the repo */}
-                                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white font-bold text-lg">Code</button>
+                                    <button className="text-center rounded-lg px-4 py-3 m-2 border-2 border-yellow text-yellow hover:bg-yellow hover:text-green">Code</button>
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* previosu work experience */}
+                {/* previous work experience */}
                 <div></div>
             </div>
         </div>
